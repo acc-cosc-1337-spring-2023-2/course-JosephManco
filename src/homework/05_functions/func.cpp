088ef3cd475818dd5441fd12b0cc9bcbe67f903a
JosephManco <123131967+JosephManco@users.noreply.github.com>
2023-03-03 04:49:33 +0000

    HW5

--- src/homework/05_functions/func.cpp
+++ src/homework/05_functions/func.cpp
@@ -1,3 +1,92 @@
 //add include statements
+#include "func.h"
+double get_gc_content(const string& dna)
+{
+    double result = 0.0;
+    int length = dna.length();
+    double gc_count = 0;
+    for(int i = 0; i < length; i++)
+    {
+        char count = dna[i];
+        if (count == 'G' || count == 'C')
+        {
+            gc_count++;
+        }
+    }
+        result = gc_count / length;
+        return result;
+}
+string reverse_string(string dna)
+{
+    string rev = "";
+
+    for(int i = dna.length() -1; i >= 0; i--)
+    {
+        rev = rev + dna[i];
+    }
+    return rev;
+    
+}
+
+string get_dna_complement(string dna1)
+{
+    dna1 = reverse_string(dna1);
+    for(size_t i = 0; i < dna1.length(); i++)
+    {
+      switch (dna1[i])
+      {
+        case 'A':
+        dna1[i] = 'T';
+        break;
+        case 'T':
+        dna1[i] = 'A';
+        break;
+        case 'G':
+        dna1[i] = 'C';
+        break;
+        case 'C':
+        dna1[i] = 'G';
+        break;
+      default:
+        break;
+      }
+    }
+    return dna1;
+}
+void display_menu()
+{
+    cout<<"\n Menu \n";
+    cout<<"\n";
+    cout<<"1.Get GC Content\n";
+    cout<<"2.Get DNA Complement\n";
+    cout<<"3.Exit\n";
+    cout<<"\n";
+}
+void menu(int choice)
+{
+    string dna = "";
+    string dna1;
+        switch(choice)
+        {
+            case 1: 
+                cout<<"Enter DNA string: ";
+                cin>> dna;
+                cout<<"The GC content is "<< get_gc_content(dna);
+
+                //cout<<"Will run GC content Function \n";
+            break;
+            case 2:
+                cout<<"Enter DNA string: ";
+                cin>> dna1;
+                cout<<"The DNA complement is "<< get_dna_complement(dna1);
+            break;
+            case 3:
+                cout<<"Exiting";
+            break;
+            default:
+                cout<<"Please Select Valid option \n";
+            break;
+        }
+}
 
 //add function code here
--- src/homework/05_functions/main.cpp
+++ src/homework/05_functions/main.cpp
@@ -1,14 +1,15 @@
-/*
-use a vector of int with values 8, 4, 20, 88, 66, 99
+#include "func.h"
 
-Prompt user for 1 for Get Max from vector and 2 for Get primes.
-Prompt user for a number and return max value or list of primes
-and display them to screen.
-Program continues until user decides to exit.
-
-*/
 int main() 
 {
-
+	auto choice = 0;
+	do
+	{
+		display_menu();
+		cout<<"Enter Menu Option ";
+		cin>>choice;
+		menu(choice);
+	}
+	while(choice !=3);
 	return 0;
 }
